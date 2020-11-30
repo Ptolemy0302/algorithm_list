bab9c938c9c46f102ea49b42955ea4c1f32b9c6f
Ptolemy0302 <ptolemy0302@163.com>
2020-11-30 22:30:27 +0800

    第一版

--- Maynooth/problem17.cpp
+++ Maynooth/problem17.cpp
@@ -2,40 +2,52 @@
 #include <iostream>
 
 using namespace std;
+#define Len 15 //所需长度为26，测试用例长度为15
 
-int B[10]; //存放答案；
-int A[10];
-// A[] = { 1, 2, 3, 8, 4, 5, 6, 0, 0, 10, 0, 7, 9, 10, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 5, 2 };
+//int A[Len] = {1, 2, 3, 8, 4, 5, 6, 0, 0, 10, 0, 7, 9, 10, 1, 0, 0, 0, 0, 1, 2, 0, 0, 0, 5, 2};
+int A[Len] = {1, 2, 3, 8, 4, 5, 6, 0, 0, 10, 0, 7, 9, 0, 5};
+int B[Len] = {0}; //存放每步计算后的答案
 
-int FindM(int n)
+int FindM(int n)//检查当前结点的左右节点的值后，加上最大值并返回
 {
     int left, right;
-    int value = A[n];
-
-    if (A[n] != NULL)
+    if (n < Len)
     {
+        int value = A[n];
         left = FindM(2 * n + 1);
         right = FindM(2 * n + 2);
         if (left > right)
             value += left;
         else
             value += right;
-    }
 
-    return value;
+        if (value > B[n])
+            B[n] = value;
+        return value;
+    }
+    else
+        return 0;
 }
-//the maximum sum is 18 an the maximum sum path is [1, 3, 5, 9].
 
 int main()
 {
-    int A[5];
-    printf("the maximum sum is %d an the maximum sum path is [", FindM(0));
+    printf("The maximum sum is %d an the maximum sum path is [", FindM(0));
     int i = 0;
-    while(B[i]!=0){
-        printf("%d", B[i]);
-        i++;
+    while (B[i] != 0)
+    {
+        if (B[2 * i + 1] > B[2 * i + 2])
+        {
+            printf("%d", B[i] - B[2 * i + 1]);
+            i = 2 * i + 1;
+        }
+        else
+        {
+            printf("%d", B[i] - B[2 * i + 2]);
+            i = 2 * i + 2;
+        }
+        if (B[i] != 0)
+            printf(",");
     }
-    printf("]");
-
+    printf("].");
     return 0;
 }
--- Maynooth/problem18.cpp
+++ Maynooth/problem18.cpp
@@ -1,12 +1,43 @@
 #include <cstdio>
 #include <iostream>
 
+int S[3] = {1, 2, 3};
+int N = 4;
+int B[10] = {}; //保存答案
+
 using namespace std;
+void showNum(int num, int n)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        if (S[i] + n == N)
+        {
+            B[num] = S[i];
+            int j = 1;
+            printf("{ %d", B[0]);
+            while (B[j] != 0)
+            {
+                printf(", %d", B[j]);
+                j++;
+            }
+            printf(" }");
+            printf("\n");
+            B[num] = 0;
+            return;
+        }
+        else if (S[i] + n < N)
+        {
+            B[num] = S[i];
+            showNum(num + 1, (S[i] + n));
+        }
+        else
+            break;
+    }
+}
 
-int main(){
-    int S[5];
-    int length;
-    int N;
-
+int main()
+{
+    //scanf("%d", N);
+    showNum(0, 0);
     return 0;
 }
