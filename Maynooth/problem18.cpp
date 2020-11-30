65fdf59f498f6410d759bd168171339d0261cf06
Ptolemy0302 <ptolemy0302@163.com>
2020-12-01 00:08:32 +0800

    未完成的代码

--- Maynooth/problem18.cpp
+++ Maynooth/problem18.cpp
@@ -1,9 +1,11 @@
 #include <cstdio>
 #include <iostream>
+#define LEN 3
 
-int S[3] = {1, 2, 3};
 int N = 4;
-int B[10] = {}; //保存答案
+int S[LEN] = {1, 2, 3};
+int B[LEN][LEN + 1] = {{0}}; //保存答案,便于进行筛选
+int C[LEN + 1] = {0};            //保存临时答案
 
 using namespace std;
 void showNum(int num, int n)
@@ -12,22 +14,34 @@ void showNum(int num, int n)
     {
         if (S[i] + n == N)
         {
-            B[num] = S[i];
-            int j = 1;
-            printf("{ %d", B[0]);
-            while (B[j] != 0)
-            {
-                printf(", %d", B[j]);
-                j++;
+            for (int i = 0;i<LEN;i++){
+                if(B[i][0] == C[0])//比较两种方案是否一样
+                {
+
+                }
+                else if(B[i][0]==0)//新增方案
+                {
+
+                }
             }
-            printf(" }");
-            printf("\n");
-            B[num] = 0;
+
+            if (B[0][0] = 0)
+            {
+                if (B[C[i]] == 0)
+                {
+                    B[i][C[i]]++;
+                    B[i][0]++;
+                }
+                else
+                {
+                    B[i][C[i]]++;
+                }
+            }
             return;
         }
         else if (S[i] + n < N)
         {
-            B[num] = S[i];
+            C[i]++;
             showNum(num + 1, (S[i] + n));
         }
         else
@@ -39,5 +53,17 @@ int main()
 {
     //scanf("%d", N);
     showNum(0, 0);
+
+    int i = 0;
+    B[i][0] = S[i];
+    int j = 1;
+    printf("{ %d", B[0]);
+    while (B[j] != 0)
+    {
+        printf(", %d", B[j]);
+        j++;
+    }
+    printf(" }");
+    printf("\n");
     return 0;
 }
